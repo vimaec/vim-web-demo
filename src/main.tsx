7d67f71b698b820ba37d4af17a730d50b1b97461
simon <simon.roberge@vimaec.com>
2022-06-08 14:20:42 -0400

    updated package

---
+++ src/main.tsx
@@ -0,0 +1,53 @@
+//import { settingsGui } from './settingsGui'
+import * as VIM from 'vim-webgl-viewer'
+import * as VR from  'vim-webgl-component'
+import Stats from 'stats-js'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import 'vim-webgl-component/dist/style.css';
+
+// Parse URL
+const params = new URLSearchParams(window.location.search)
+let url = params.has('vim')
+  ? params.get('vim')
+  : 'https://vim.azureedge.net/samples/residence.vim'
+
+url = params.has('model')
+? params.get('model')
+: 'https://vim.azureedge.net/samples/residence.vim'
+
+// Parse Transparency
+let transparency = 'all' as VIM.Transparency.Mode
+if (params.has('transparency')) {
+  const t = params.get('transparency')
+  transparency = VIM.Transparency.isValid(t) ? t : 'all'
+}
+
+ReactDOM.render(<VR.VimComponent onViewerReady={onViewerReady}/>, VR.createRoot())
+
+function onViewerReady(viewer : VIM.Viewer){
+  viewer.loadVim(
+    url,
+    {
+      rotation: { x: 270, y: 0, z: 0 },
+      transparency : transparency
+    } 
+  ).then(_ => console.log("Vim Successfully loaded"))
+}
+
+const stats = new Stats()
+stats.dom.style.top = '84px'
+stats.dom.style.left = '16px'
+document.body.appendChild(stats.dom)
+animate()
+
+function animate () {
+  requestAnimationFrame(() => animate())
+
+  if (stats) {
+    stats.update()
+  }
+}
+
+
+
--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "./docs/dev",
+    outDir: "./docs",
     //emptyOutDir: true,
-
     sourcemap: true,
-    /*
-    lib: {
-      formats: ["iife"],
-      entry: "./src/vimWebApp.ts",
-      name: "vim",
-    },
-    */
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
     // Minify set to true will break the IIFE output
     minify: false,
   },
