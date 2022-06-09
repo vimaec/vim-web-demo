a1d14da02876430facdc922ebffd4ef67e6e50be
simon <simon.roberge@vimaec.com>
2022-06-09 12:37:32 -0400

    configs

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "./docs",
+    outDir: "./docs/dev",
     //emptyOutDir: true,
     sourcemap: true,
     // Minify set to true will break the IIFE output
     minify: false,
+
+    // This makes build file names look nice. (and trackable in github)
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
   },
 });
