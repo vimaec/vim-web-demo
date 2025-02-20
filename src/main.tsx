982489491ba8ec8b503285e288f038969f822955
Martin Ashton <martin.ashton@vimaec.com>
2025-02-20 18:05:20 -0500

    Updated to consume vim-web package

--- src/main.tsx
+++ src/main.tsx
@@ -1,57 +1,46 @@
-import 'vim-webgl-component/dist/style.css';
-import {THREE, VIM, createVimComponent, getLocalSettings } from 'vim-webgl-component'
-import { Object3D } from 'vim-webgl-viewer';
+import React, { useEffect, useRef } from 'react'
+import { WebglReact } from 'vim-web'
+import { createRoot } from 'react-dom/client'
+import 'vim-web/dist/style.css';
 
-// Parse URL
-const params = new URLSearchParams(window.location.search)
-let url = params.has('vim') || params.has('model')
-  ? params.get('vim') ?? params.get('model') 
-  : 'https://vim.azureedge.net/samples/residence.v1.2.75.vim'
-
-// Parse Transparency
-let transparency = 'all' as VIM.Transparency.Mode
-if (params.has('transparency')) {
-  const t = params.get('transparency')
-  transparency = VIM.Transparency.isValid(t) ? t : 'all'
+function getPathFromUrl () {
+  const params = new URLSearchParams(window.location.search)
+  return params.get('vim') ?? 'https://storage.cdn.vimaec.com/samples/residence.v1.2.75.vim'
 }
 
-// Parse Dev mode
-let devMode = false
-if (params.has('dev')) {
-  const t = params.get('dev')
-  devMode =  t === 'true'
-}
+async function createComponent (div: HTMLDivElement, ref: React.MutableRefObject<WebglReact.Refs.VimComponentRef>) {
+  const webgl = await WebglReact.createWebglComponent(div)
+  ref.current = webgl
+  globalThis.viewer = webgl
 
-demo()
-async function demo () {
-  const cmp = await createVimComponent(undefined, getLocalSettings())
+  const url = getPathFromUrl()
+  const request = webgl.loader.request({ url } )
 
-  const request = await cmp.loader.request({
-    url: url ?? 'https://vim.azureedge.com/samples/Wolford_Residence.r2025.vim'
-  }, {
-    rotation: new THREE.Vector3(270, 0, 0)
-  })
-
-  for await (const progress of request.getProgress()) {
-    console.log(`Downloading Vim (${(progress.loaded / 1000).toFixed(0)} kb)`)
-  }
   const result = await request.getResult()
-  if (result.isError()) {
-    console.error(result.error)
-    return
+  if (result.isSuccess()) {
+    webgl.loader.add(result.result)
+    webgl.camera.frameVisibleObjects()
   }
-  const vim = result.result
-  cmp.loader.add(vim)
-
-  globalThis.THREE = THREE
-  globalThis.component = cmp
-  globalThis.vim = vim
-
-  // Example: select a specific element in the file and log its bimElement data to the console
-  // var [obj] = vim.getObjectsFromElementId(348218) as Object3D[]
-  // cmp.viewer.selection.add(obj)
-  // var bimElement = await obj.getBimElement()
-  // console.log(bimElement)
 }
 
+export function VimWebComponent () {
+  const div = useRef<HTMLDivElement>(null)
+  const cmp = useRef<WebglReact.Refs.VimComponentRef>()
+  useEffect(() => {
+    createComponent(div.current, cmp)
+    return () => cmp.current?.dispose()
+  }, [])
 
+  return (
+    <div ref={div} className='vc-inset-0 vc-absolute'/>
+  )
+}
+
+function App () {
+  return <VimWebComponent/>
+}
+
+const container = document.getElementById('root')
+if (!container) throw new Error('No container found')
+const root = createRoot(container)
+root.render(<App/>)
