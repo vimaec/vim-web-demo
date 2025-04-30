1cfd4e70e398f641d99c2cdf228e1f77c18205a5
vim-sroberge <simon.roberge@vimaec.com>
2025-04-30 14:48:48 -0400

    Updates

--- replace.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-const file = "index.html";
-fs.readFile(file, "utf8", (err, data) => {
-  const regex = new RegExp("<title>VIM Viewer ?([0-9]*)</title>");
-  const matches = data.match(regex);
-  const number = Number.parseInt(matches[1]) + 1;
-  const result = data.replace(regex, `<title>VIM Viewer ${number}</title>`);
-  fs.writeFile(file, result, () => {});
-});
