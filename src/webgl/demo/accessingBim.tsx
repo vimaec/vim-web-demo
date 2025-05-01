a89a407b0e6dad195ba3f919ad25789544d4decf
vim-sroberge <simon.roberge@vimaec.com>
2025-05-01 14:18:28 -0400

    Added demo for category name and familyType name

--- src/webgl/demo/accessingBim.tsx
+++ src/webgl/demo/accessingBim.tsx
@@ -12,11 +12,27 @@ export function AccessingBim () {
       const bimElement = await first?.getBimElement()
       if (!bimElement) return
       console.log("Element Id:", bimElement.id)
-      console.log("Element Name", bimElement.name)
+      console.log("Element Name:", bimElement.name)
       console.log("Element Type:", bimElement.type)
-      console.log("Family Name:", bimElement.familyName)
+      
+      // Get the category from the category table
+      // Direct accessors such as element.category are no longer available
+      const category = await vim.bim.category.get(bimElement.categoryIndex)
+      console.log("Element Category:", category.name)
 
-      console.log(bimElement)
+      // Get the level information from the level table
+      // Then back to element, because a level is also an element
+      const level = await vim.bim.level.get(bimElement.levelIndex)
+      const levelElement = await vim.bim.element.get(level.elementIndex)
+      console.log("Element Level:", levelElement.name)
+
+      // For family Type, you need to find the family related to the element at hand first
+      // Once you have the family instance, you got to family type, and then back to element to get the name
+      const familyInstances = await vim.bim.familyInstance.getAll()
+      const familyInstance = familyInstances.find(fi => fi.elementIndex === bimElement.index)
+      const familyType = await vim.bim.familyType.get(familyInstance.familyTypeIndex)
+      const familyTypeElement = await vim.bim.element.get(familyType.elementIndex)
+      console.log("Family Type Name:", familyTypeElement.name)
     })
   })
 }
